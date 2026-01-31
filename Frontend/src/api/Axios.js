b3b32d9f3f2720f07961f5e51191244e6da6856b
udaytejan <satyateja1707@gmail.com>
2026-02-01 00:23:03 +0530

    Remove CORS variable

--- Frontend/src/api/Axios.js
+++ Frontend/src/api/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: (import.meta.env.VITE_API_BASE_URL || "https://escout-esports-scouting-platform-1.onrender.com") + "/api/v1/users",
+  baseURL: "https://escout-esports-scouting-platform-1.onrender.com/api/v1/users",
   withCredentials: true,
 });
 
