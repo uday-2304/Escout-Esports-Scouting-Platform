0d265f168e2f49a417b29bc3c58dcc134e22c64d
udaytejan <satyateja1707@gmail.com>
2026-02-01 00:01:56 +0530

    Allow all origins for CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "https://escoutesports.vercel.app",
+    origin: true, // Allow all origins dynamically
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
